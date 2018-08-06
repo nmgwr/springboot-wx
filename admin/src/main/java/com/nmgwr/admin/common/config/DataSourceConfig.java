7294266c9e36cd25b6a8cc5a4a7ab11a1e59e94a
nmgwr <nmgwr1216@163.com>
2018-08-06 15:43:39 +0800

    更换数据源为druid，以前那个不稳定。。。

--- admin/src/main/java/com/nmgwr/admin/common/config/DataSourceConfig.java
+++ admin/src/main/java/com/nmgwr/admin/common/config/DataSourceConfig.java
@@ -1,21 +1,47 @@
 package com.nmgwr.admin.common.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 public class DataSourceConfig {
-    @Bean(name = "dataSource")
+
+    @Bean     //声明其为Bean实例
+    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
     public DataSource dataSource(Environment env){
-        HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
-        ds.setUsername(env.getProperty("spring.datasource.username"));
-        ds.setPassword(env.getProperty("spring.datasource.password"));
-        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        return ds;
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(env.getProperty("spring.datasource.url"));
+        datasource.setUsername(env.getProperty("spring.datasource.username"));
+        datasource.setPassword(env.getProperty("spring.datasource.password"));
+        datasource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+
+        //configuration
+        datasource.setInitialSize(Integer.parseInt(env.getProperty("spring.datasource.initialSize")));
+        datasource.setMinIdle(Integer.parseInt(env.getProperty("spring.datasource.minIdle")));
+        datasource.setMaxActive(Integer.parseInt(env.getProperty("spring.datasource.maxActive")));
+        datasource.setMaxWait(Integer.parseInt(env.getProperty("spring.datasource.maxWait")));
+        datasource.setTimeBetweenEvictionRunsMillis(Integer.parseInt(env.getProperty("spring.datasource.timeBetweenEvictionRunsMillis")));
+        datasource.setMinEvictableIdleTimeMillis(Integer.parseInt(env.getProperty("spring.datasource.minEvictableIdleTimeMillis")));
+        datasource.setValidationQuery(env.getProperty("spring.datasource.validationQuery"));
+        datasource.setTestWhileIdle(Boolean.parseBoolean(env.getProperty("spring.datasource.testWhileIdle")));
+        datasource.setTestOnBorrow(Boolean.parseBoolean(env.getProperty("spring.datasource.testOnBorrow")));
+        datasource.setTestOnReturn(Boolean.parseBoolean(env.getProperty("spring.datasource.testOnReturn")));
+        datasource.setPoolPreparedStatements(Boolean.parseBoolean(env.getProperty("spring.datasource.poolPreparedStatements")));
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(Integer.parseInt(env.getProperty("spring.datasource.maxPoolPreparedStatementPerConnectionSize")));
+        datasource.setUseGlobalDataSourceStat(Boolean.parseBoolean(env.getProperty("spring.datasource.useGlobalDataSourceStat")));
+        try {
+            datasource.setFilters(env.getProperty("spring.datasource.filters"));
+        } catch (SQLException e) {
+            System.err.println("druid configuration initialization filter: "+ e);
+        }
+        datasource.setConnectionProperties(env.getProperty("spring.datasource.connectionProperties"));
+        return datasource;
     }
+
 }
